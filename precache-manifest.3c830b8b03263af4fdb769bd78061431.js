f580f90234dc4cb9f70d982cbea0add4499f0a1e
Divyansh Sharma <73015446+div360@users.noreply.github.com>
2022-05-03 07:20:19 +0530

    Updates

---
+++ precache-manifest.3c830b8b03263af4fdb769bd78061431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2aac6b39b20da34cfd5c",
+    "url": "/TextUtilities-React/static/css/main.98a12b40.chunk.css"
+  },
+  {
+    "revision": "2aac6b39b20da34cfd5c",
+    "url": "/TextUtilities-React/static/js/main.2aac6b39.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtilities-React/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "284c82effe6d3f94e3ec",
+    "url": "/TextUtilities-React/static/js/2.284c82ef.chunk.js"
+  },
+  {
+    "revision": "d90e533994359f0c4d41",
+    "url": "/TextUtilities-React/static/js/runtime~main.d90e5339.js"
+  },
+  {
+    "revision": "7c1bcdd0ab8cc4959390430b482025e0",
+    "url": "/TextUtilities-React/index.html"
+  }
+];
